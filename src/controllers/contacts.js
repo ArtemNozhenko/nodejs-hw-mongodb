47b340b11727ad7bf5ced3b694dfb7e5acb6d37e
ArtemNozhenko <artem2516nozh@gmail.com>
2024-07-21 21:41:59 +0200

    fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -54,7 +54,7 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: `Successfully patched a contact!`,
-    data: result.contact,
+    data: result,
   });
 };
 
