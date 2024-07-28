b957f746ed5dba513b70c11a4f22e014fc2cc3bd
ArtemNozhenko <artem2516nozh@gmail.com>
2024-07-28 17:42:36 +0200

    validation

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,25 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortOrder = (sortOrder) => {
+  if ([SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder)) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+  const keys = ['_id', 'name'];
+
+  if (keys.includes(sortBy)) return sortBy;
+  return '_id';
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortOrder = parseSortOrder(sortOrder);
+  const parsedSortBy = parseSortBy(sortBy);
+
+  return {
+    sortBy: parsedSortBy,
+    sortOrder: parsedSortOrder,
+  };
+};
